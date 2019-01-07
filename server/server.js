7b1d31c20eeecdcea1ac08eebc1002cc92daa99a
jockeyjo <praveenkumarmcoengg1996@gmail.com>
2019-01-06 22:29:18 -0800

    new project-1

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const path = require ('path');
+const http = require ('http');
 const express = require ('express');
+const socketIO = require ('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
-app.listen(3000, () => {
-  console.log('hey man our port is started');
+
+io.on('connection',(socket) =>{
+  console.log('new user has been connected');
+
+  socket.on('disconnect',() => {
+    console.log('user was disconnected');
+  });
 });
-//console.log(__dirname + '/../public');
+server.listen(port, () => {
+  console.log(`server is up on ${port}`);
+});
