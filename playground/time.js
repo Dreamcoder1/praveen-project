57f8b0938c665d3d228608411b8da37480c68463
jockeyjo <praveenkumarmcoengg1996@gmail.com>
2019-01-10 00:09:17 +0530

    new stylish chat output

--- playground/time.js
+++ playground/time.js
@@ -1,8 +1,7 @@
-var moment = require('moment');
+//var moment = require('moment');
 
 //var date = moment();
 //console.log(date.format('MMM Do, YYYY'))
 
-var createdAt = 1234;
-var date = moment(createdAt);
-console.log(date.format('h:mm a'))
+//var date = moment(createdAt);
+//console.log(date.format('h:mm a'))
